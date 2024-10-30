0fdda45bd918463d0357be149b786ca9e416b683
donfortune <donfortunet.df@gmail.com>
2024-10-30 13:25:19 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import logging
+import time
+import random
+
+
+app = Flask(__name__)
+
+#set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@app.route('/')
+def home():
+    logger.info('Home page is accessed')    
+    return "Welcome to the logging app!"
+
+@app.route("/generate-log")
+def generate_log():
+    log_level = random.choice([logging.INFO, logging.WARNING, logging.ERROR])
+    logger.log(log_level, f"Log level {log_level} generated at {time.strftime('%Y-%m-%d %H:%M:%S')}")
+    return "Log generated!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
+
---
+++ dockerfile
@@ -0,0 +1,8 @@
+
+
+FROM python:3.10.11
+WORKDIR /app
+COPY . /app
+RUN pip3 install -r requirements.txt
+EXPOSE 5001
+CMD ["python3", "app.py"]
